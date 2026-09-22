ca9be686b99433e9d3d788d7090399603beffd74
stvrony <lstaron13@gmail.com>
2026-09-22 18:09:46 +0200

    Framework Functions Declaration

--- VisualGitSetup/VisualGitSetup/mainSetup.cpp
+++ VisualGitSetup/VisualGitSetup/mainSetup.cpp
@@ -2,9 +2,19 @@
 
 using namespace std;
 
+void Initialize();
+
+void GetInput();
+void Update();
+void Render();
+
+void Shutdown();
+
 
 int main()
 {
 	cout << "Hello World!" << endl;
 	return 0;
 }
+
+
